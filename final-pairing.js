88c7d944f1bda22385e8a643612e0ad54118ff49
Steve Thomas <blackchicagoite@mail.com>
2018-08-27 12:04:33 -0400

    Update final-pairing.js

--- final-pairing.js
+++ final-pairing.js
@@ -85,7 +85,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "password",
+    password: "amazon3210",
     database: "tct2018"
 });
 
