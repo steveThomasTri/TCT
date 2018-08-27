4fe5412756971d3d711607ce3309bbb133e894a6
Black Irish <blackirishlibenforcer@protonmail.com>
2018-08-27 11:52:12 -0400

    revised api and final pairing

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -134,12 +134,11 @@ module.exports = function (app, passport) {
     //compare ratings for all pairings
     app.get("/pairingcompare/:TID/:round", function (req, res) {
         var compared = [];
-        connection.query("SELECT id from games where tournament_id = ? LIMIT 1", [req.params.TID], function (err, result) {
-            connection.query("SELECT id, player1, player2 from tournament_pairings where round = ? and tournament_id = ?", [req.params.round, result[0].id], function (err, result2) {
+        connection.query("SELECT tournament_id from games where tournament_id = ? LIMIT 1", [req.params.TID], function (err, result) {
+            connection.query("SELECT id, player1, player2 from tournament_pairings where round = ? and tournament_id = ?", [req.params.round, result[0].tournament_id], function (err, result2) {
                 async.each(result2, function (pairing, callback) {
                     connection.query("SELECT games.game, game_ratings.game_id, game_ratings.rating from games join game_ratings on games.id=game_ratings.game_id where player_id = ?", [pairing.player1], function (err, result3) {
                         connection.query("SELECT games.game, game_ratings.game_id, game_ratings.rating from games join game_ratings on games.id=game_ratings.game_id where player_id = ?", [pairing.player2], function (err, result4) {
-
                             for (var i = 0; i < result3.length; i++) {
                                 result3[i].difference = Math.abs(result3[i].rating - result4[i].rating)
                                 result3[i].avg = (result3[i].rating + result4[i].rating) / 2;
